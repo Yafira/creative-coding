1f56b19920068686bca51540fe2ea4c16b4f52d2
Yafira <yafira.martinez@lc.cuny.edu>
2020-06-08 19:19:39 -0400

    setting up Two.js

---
+++ Spinning Square/script.js
@@ -0,0 +1,16 @@
+// pick a tag to do the code in
+// then set up Two.js
+
+const container = document.querySelector("section")
+
+
+const params = { 
+    width: 500,
+    height: 500
+}
+
+
+const two = new Two(params)
+two.appendTo(container)
+
+
