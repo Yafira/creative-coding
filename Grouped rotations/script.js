415f26566a4c9cad9a51a734796121afb86ee6d2
Yafira <yafira.martinez@lc.cuny.edu>
2020-06-09 19:00:48 -0400

    added rotation to shapes

--- Grouped rotations/script.js
+++ Grouped rotations/script.js
@@ -7,22 +7,36 @@ const params = {
     height: 500
 }
 
-
 const two = new Two(params)
 two.appendTo(container)
 
 const numberOfShapes = 12
 const plotRadius = 150
 
+const shapes = []
+
 
 for (let i = 0; i < numberOfShapes; i = i + 1) {
     const angle = fullRotation * i / numberOfShapes
+
     const x = 250 + plotRadius * Math.cos(angle)
     const y = 250 + plotRadius * Math.sin(angle)
+
     const shape = two.makeRectangle(x, y, 50, 50)
     shape.noStroke()
     shape.fill = "#e5adf5"
+    shape.rotation = angle
+
+
+    shapes.push(shape)
 }
+
+
+two.bind("update", function () {
+    shapes.forEach(shape => {
+        shape.rotation += 0.025
+    })
+})
 
 
 two.play()
