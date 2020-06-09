a7eb142a2977492a5feeee4bbd31fda418800fa9
Yafira <yafira.martinez@lc.cuny.edu>
2020-06-09 18:44:48 -0400

    implemented a for loop to add multiple shapes

--- Grouped rotations/script.js
+++ Grouped rotations/script.js
@@ -11,7 +11,19 @@ const params = {
 const two = new Two(params)
 two.appendTo(container)
 
-const shape = two.makeRectangle(250, 250, 100, 100)
+const numberOfShapes = 12
+const plotRadius = 150
+
+
+for (let i = 0; i < numberOfShapes; i = i + 1) {
+    const x = i * 30 + 30
+    const y = 250
+    const shape = two.makeRectangle(x, y, 20, 20)
+    shape.noStroke()
+    shape.fill = "#e5adf5"
+}
+
+
 
 
 two.play()
