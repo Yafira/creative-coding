8c2b048004f1a0e42729f522260edb96b9053abf
Yafira <yafira.martinez@lc.cuny.edu>
2020-06-10 17:41:58 -0400

    project setup + utils.js

---
+++ Looping squares/script.js
@@ -0,0 +1,25 @@
+const container = document.querySelector("section")
+
+const params = { 
+  width: 500, 
+  height: 500 
+}
+
+const two = new Two(params)
+two.appendTo(container)
+
+// config for animation
+const numberOfShapes = 40
+const shapes = []
+
+// make shapes
+for (let i = 0; i < numberOfShapes; i++) { 
+  
+}
+
+two.bind("update", function () {
+  // draw
+})
+
+two.play()
+
