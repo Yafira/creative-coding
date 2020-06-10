d6315808de6f88cdc50a1628338e69f03e3238d6
Yafira <yafira.martinez@lc.cuny.edu>
2020-06-10 19:18:24 -0400

    added a shape => square

--- Looping squares/script.js
+++ Looping squares/script.js
@@ -10,11 +10,17 @@ two.appendTo(container)
 
 // config for animation
 const numberOfShapes = 40
+const shapeIncr = 20
 const shapes = []
 
 // make shapes
 for (let i = 0; i < numberOfShapes; i++) { 
-  
+  const size = (numberOfShapes - i) * shapeIncr
+  const shape = two.makeRectangle(250, 250, size, size)
+  shape.fill = "#b583ee"
+  shape.noStroke()
+
+  shapes.push(shape)
 }
 
 two.bind("update", function () {
