527551537a5cdd61b696283845aec8533b594c1d
Yafira <yafira.martinez@lc.cuny.edu>
2020-06-09 20:05:54 -0400

    added variation 3: grow and shrink scale

--- Grouped rotations/script.js
+++ Grouped rotations/script.js
@@ -10,10 +10,13 @@ const params = {
 const two = new Two(params)
 two.appendTo(container)
 
-const numberOfShapes = 12
+// const numberOfShapes = 12
 
-// variation 2: stick version
+// Variation 2: Stick version
 // const numberOfShapes = 40
+
+// Variation 3: Grow and shrink scale
+const numberOfShapes = 40
 
 const plotRadius = 150
 
@@ -26,10 +29,13 @@ for (let i = 0; i < numberOfShapes; i = i + 1) {
     const x = plotRadius * Math.cos(angle)
     const y = plotRadius * Math.sin(angle)
 
-    const shape = two.makeRectangle(x, y, 50, 50)
+    // const shape = two.makeRectangle(x, y, 50, 50)
 
-    // variation 2: stick version
+    // variation 2: Stick version
     // const shape = two.makeRectangle(x, y, 150, 10)
+
+    // variation 3: Grow and shrink scale
+    const shape = two.makeRectangle(x, y, 150, 10)
 
     shape.noStroke()
     shape.fill = "#e5adf5"
@@ -42,19 +48,40 @@ for (let i = 0; i < numberOfShapes; i = i + 1) {
 const group = two.makeGroup(shapes)
 group.translation.set(250, 250)
 
-
+    // Variation 3: Grow and shrink scale
+    let scaler = 1
+    let scaling = "grow"
 
 
 two.bind("update", function () {
     group.rotation += 0.005
 
-    // variation 2: stick version
+    // Variation 2: Stick version
     // group.rotation += 0.006125
+
+    // Variation 3: Grow and shrink scale
+    if (scaling === "grow") {
+        scaler += 0.005
+    }
+
+    if (scaling === "shrink") {
+        scaler -= 0.005
+    }
+
+    if (scaler > 3) {
+        scaling = "shrink"
+    }
+
+    if (scaler < 0.5) {
+        scaling = "grow"
+    }
 
 
     shapes.forEach(shape => {
         shape.rotation += 0.025
 
+        // Variation 3: Grow and shrink scale
+        shape.scale = scaler 
     })
 })
 
