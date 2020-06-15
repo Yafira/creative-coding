942982021d785e3fd675d19c86d32bcc609b8046
Yafira <yafira.martinez@lc.cuny.edu>
2020-06-15 17:31:09 -0400

    setting up a general timeline

--- Random placements/script.js
+++ Random placements/script.js
@@ -11,6 +11,7 @@ two.appendTo(container)
 // config for our animation
 const numberOfShapes = 25
 const shapes = []
+const loopDuration = 6 * 60
 
 // make shapes
 for (let i = 0; i < numberOfShapes; i++) {
@@ -26,8 +27,16 @@ for (let i = 0; i < numberOfShapes; i++) {
   shapes.push(shape)
 }
 
-two.bind("update", function () {
+two.bind("update", function (frameCount) {
   // draw
+  const currentFrame = frameCount % loopDuration
+  const t = currentFrame / loopDuration
+
+  shapes.forEach((shape, i) => {
+    const x = mapAndClamp(t, 0, 1, 150, 350)
+    shape.translation.x = x 
+  })
+
 })
 
 two.play()
